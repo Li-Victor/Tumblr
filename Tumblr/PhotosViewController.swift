0a8fc272b3b5c9cbcbabc4a43db4999a63aba471
Victor Li <victorli5674@yahoo.com>
2018-08-29 23:32:34 -0700

    building the photo feed

--- Tumblr/PhotosViewController.swift
+++ Tumblr/PhotosViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var posts: [[String: Any]] = []
 
+    @IBOutlet weak var photoTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        photoTableView.delegate = self
+        photoTableView.dataSource = self
 
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -27,6 +33,7 @@ class PhotosViewController: UIViewController {
                 // Get the dictionary from the response key
                 let responseDictionary = dataDictionary["response"] as! [String: Any]
                 self.posts = responseDictionary["posts"] as! [[String: Any]]
+                self.photoTableView.reloadData()
             }
         }
         task.resume()
@@ -38,6 +45,24 @@ class PhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        let post = self.posts[indexPath.row]
+        
+        if let photos = post["photos"] as? [[String: Any]] {
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            cell.photoImageView.af_setImage(withURL: url!)
+        }
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
