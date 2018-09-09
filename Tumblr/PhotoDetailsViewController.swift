f9d0d519e2af71b3d6ec8cf865ec896553ac8d15
Victor Li <victorli5674@yahoo.com>
2018-09-09 00:23:49 -0700

    resize iamge

--- Tumblr/PhotoDetailsViewController.swift
+++ Tumblr/PhotoDetailsViewController.swift
@@ -22,24 +22,11 @@ class PhotoDetailsViewController: UIViewController {
             let photoURL = URL(string: post.photoPath)!
             photoImageView.af_setImage(withURL: photoURL)
         }
-        
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
